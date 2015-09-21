b56c4b8f7c05fea8edf3ebcf4ef65e4ee14dbbce
Keene <14785@students.isb.ac.th>
2015-09-21 20:24:55 +0700

    Added Base Project Folder
    This is the application

---
+++ ISB Ace/ISB Ace/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ISB Ace
+//
+//  Created by ISB Dev Team NB2K16 on 9/21/15.
+//  Copyright © 2015 HS Software Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Test")
+        // Main method
+        }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Don't touch this, n
+    }
+
+
+}
+
---
+++ ISB Ace/ISB AceTests/ISB_AceTests.swift
@@ -0,0 +1,36 @@
+//
+//  ISB_AceTests.swift
+//  ISB AceTests
+//
+//  Created by Kake on 9/21/15.
+//  Copyright © 2015 HS Software Dev. All rights reserved.
+//
+
+import XCTest
+@testable import ISB_Ace
+
+class ISB_AceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
